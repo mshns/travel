c5795d6c9f8be5163e3041ec020a7fb95cb1b7c1
mshns <mishanos412@gmail.com>
2022-07-23 06:17:05 +0300

    fix: change login popup

--- travel/scripts/menu.js
+++ travel/scripts/menu.js
@@ -3,16 +3,10 @@ burgerMenu = document.querySelector(".burgermenu");
 menuClose = document.querySelector(".bergermenu-close");
 menuShadow = document.querySelector(".menu-shadow");
 menuItem = document.querySelectorAll(".menu-link");
-
 loginButton = document.querySelector(".header-login-button");
 accountLink = document.querySelector(".account-link");
 logIn = document.querySelector(".login-container");
-
 registerLink = document.querySelector(".register");
-signUp = document.querySelector(".sign-container");
-
-loginLink = document.querySelector(".login-link");
-
 signInButton = document.querySelector(".login-signin-button");
 logPass = document.querySelectorAll(".login-input-box");
 
@@ -35,10 +29,7 @@ menuShadow.addEventListener('click',
     menuShadow.classList.remove("active");
     burgerMenu.classList.remove("active");
     logIn.classList.remove("active");
-    signUp.classList.remove("active");
     logIn.style.transition = '';
-    signUp.style.transition = '';
-
   }
 );
 
@@ -66,24 +57,40 @@ accountLink.addEventListener('click',
   }
 );
 
+let popup = 'register';
 registerLink.addEventListener('click',
   function() {
-    logIn.style.transition = 'none';
-    signUp.style.transition = 'none';
-    logIn.classList.remove("active");
-    signUp.classList.add("active");
-  }
-);
-
-loginLink.addEventListener('click',
-  function() {
-    signUp.classList.remove("active");
-    logIn.classList.add("active");
+    if (popup === 'register') {      
+      logIn.style.transition = 'none';
+      logIn.classList.add("signin");
+      document.querySelector('.login-title').textContent = "Create account";
+      document.querySelector('.signin-buttons').style.display = "none";
+      signInButton.textContent = "Sign Up";
+      document.querySelector('.login-forgot-password').style.display = "none";
+      document.querySelector('.link-description').textContent = "Already have an account? ";
+      registerLink.textContent = "Log in";
+      document.querySelector('.login-input').style.marginTop = "8px";
+      document.querySelector('.separator').style.marginTop = "14px";
+      popup = 'logIn';
+    } else {
+      logIn.style.transition = 'none';
+      logIn.classList.remove("signin");
+      document.querySelector('.login-container').style.height = "";
+      document.querySelector('.login-title').textContent = "Log in to your account";
+      document.querySelector('.signin-buttons').style.display = "flex";
+      signInButton.textContent = "Sign In";
+      document.querySelector('.login-forgot-password').style.display = "contents";
+      document.querySelector('.link-description').textContent = "Don’t have an account? ";
+      registerLink.textContent = "Register";
+      document.querySelector('.login-input').style.marginTop = "";
+      document.querySelector('.separator').style.marginTop = "";
+      popup = 'register';
+    }
   }
 );
 
 signInButton.addEventListener('click',
   function() {
-    alert(`E-mail: ${logPass[0].value}\nPassword: ${logPass[1].value}`);
+    alert(`◼️ E-mail: ${logPass[0].value}\n◼️ Password: ${logPass[1].value}`);
   }
 );
